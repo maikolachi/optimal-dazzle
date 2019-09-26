e8bafa9e6cfd0d95bb42310edf0872fc6eeb1f82
Faisal Bhombal <fbhombal@gmail.com>
2019-09-25 21:45:12 -0500

    Created view model and moved

---
+++ Source/Optimal Dazzle/Event+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Event+CoreDataClass.swift
+//  Optimal Dazzle
+//
+//  Created by Faisal Bhombal on 9/25/19.
+//  Copyright © 2019 Bhombal. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Event)
+public class Event: NSManagedObject {
+
+}
---
+++ Source/Optimal Dazzle/MasterViewViewModel.swift
@@ -0,0 +1,84 @@
+//
+//  MasterViewViewModel.swift
+//  Optimal Dazzle
+//
+//  Created by Faisal Bhombal on 9/25/19.
+//  Copyright © 2019 Bhombal. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class MasterViewViewModel: NSObject, NSFetchedResultsControllerDelegate {
+
+    var managedObjectContext: NSManagedObjectContext? = nil
+    
+    var fetchedResultsController: NSFetchedResultsController<Event> {
+        if _fetchedResultsController != nil {
+            return _fetchedResultsController!
+        }
+        
+        let fetchRequest: NSFetchRequest<Event> = Event.fetchRequest()
+        
+        // Set the batch size to a suitable number.
+//        fetchRequest.fetchBatchSize = 20
+        
+        // Edit the sort key as appropriate.
+        let sortDescriptor = NSSortDescriptor(key: "timestamp", ascending: false)
+        
+        fetchRequest.sortDescriptors = [sortDescriptor]
+        
+        // Edit the section name key path and cache name if appropriate.
+        // nil for section name key path means "no sections".
+        let aFetchedResultsController = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: self.managedObjectContext!, sectionNameKeyPath: nil, cacheName: "Master")
+        aFetchedResultsController.delegate = self
+        _fetchedResultsController = aFetchedResultsController
+        
+        do {
+            try _fetchedResultsController!.performFetch()
+        } catch {
+            // Replace this implementation with code to handle the error appropriately.
+            // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+            let nserror = error as NSError
+            fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+        }
+        
+        return _fetchedResultsController!
+    }
+    var _fetchedResultsController: NSFetchedResultsController<Event>? = nil
+
+    @objc
+    func insertNewObject(_ sender: Any) {
+        let context = self.fetchedResultsController.managedObjectContext
+//        let newEvent = Event(context: context)
+        
+        // If appropriate, configure the new managed object.
+//        newEvent.timestamp = Date()
+        
+        // Save the context.
+        do {
+            try context.save()
+        } catch {
+            // Replace this implementation with code to handle the error appropriately.
+            // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+            let nserror = error as NSError
+            fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+        }
+    }
+    
+    func event(at indexPath: IndexPath) -> Event {
+        return self.fetchedResultsController.object(at: indexPath)
+    }
+    
+    var numberOfObjects: Int {
+        guard let section = self.fetchedResultsController.sections?.first else {
+            return 0
+        }
+        return section.numberOfObjects
+    }
+    
+    
+}
+
+
+
